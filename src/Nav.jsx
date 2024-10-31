aadc48bfa81e712df95eea9a06f52b5b5d5f4f3c
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-10-31 17:07:22 -0300

    first commit

---
+++ src/Nav.jsx
@@ -0,0 +1,19 @@
+function Nav(){
+    return(
+        <div className="imgNav">
+            <div className="nav">
+                <h4><strong>COLIBRI</strong></h4>
+                <ol>
+                    <li><a href="">HOME</a></li>
+                    <li><a href="">FEATURES</a></li>
+                    <li><a href="">PORTIFOLIO</a></li>
+                    <li><a href="">TEAM</a></li>
+                    <li><a href="">CONTACT</a></li>
+                    <li><a id="button" href="">START NOW</a></li>
+                </ol>
+            </div>
+        </div>
+    ) 
+}
+
+export default Nav
